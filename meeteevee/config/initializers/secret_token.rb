dfd0be28b226279332b2cdb295acd6cb747dc0c8
Austin Fagan <austinfagan@joe.local>
2012-05-01 13:50:18 +0200

    created meeteevee rails vanilla app

---
+++ meeteevee/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Meeteevee::Application.config.secret_token = '41875668344cf76a0e996646b4dbaa1bde921f526d9c007fefeada3a2f383fd04726f7e63897e1d036a1653b747acf889eb0c8ea0dbdfc2cf37c46749b68b2e7'
